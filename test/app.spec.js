e2b0cbbdb251d4200e0861bf9ed92b2337759832
1onlinesolution <team@damianos.io>
2021-06-12 15:50:00 +0100

    fix unit tests

--- test/app.spec.js
+++ test/app.spec.js
@@ -3,6 +3,13 @@ const assert = require('assert');
 const { HttpStatus } = require('@1onlinesolution/dws-http');
 const app = require('../app');
 
+const message = {
+  from: 'team@damianos.io',
+  to: 'team@damianos.io',
+  subject: '@1onlinesolution/dws-hermes - Testing email service',
+  text: 'Hello world',
+};
+
 describe('GET /', function () {
   let request;
   beforeEach(function () {
@@ -19,14 +26,6 @@ describe('GET /', function () {
       .set('Accept', 'application/json')
       .expect('Content-Type', /json/)
       .expect(function (res) {
-        // {
-        //   "status": 200,
-        //   "success": true,
-        //   "timestamp": "2021-06-12T09:55:07.000Z",
-        //   "value": {
-        //     "message": "Welcome to Hermes Service - OK"
-        // }
-        // }
         assert(res.body.status === HttpStatus.statusOk);
         assert(res.body.success);
         assert(res.body.value.message.includes('Hermes Service'));
@@ -47,14 +46,6 @@ describe('GET /wrong-route', function () {
       .set('Accept', 'application/json')
       .expect('Content-Type', /json/)
       .expect(function (res) {
-        // {
-        //   "status": 404,
-        //   "success": false,
-        //   "timestamp": "2021-06-12T09:55:07.000Z",
-        //   "value": {
-        //     "message": "... not found"
-        // }
-        // }
         assert(res.body.status === HttpStatus.statusNotFound);
         assert(res.body.success === false);
         assert(res.body.value.message.includes('not found'));
@@ -75,14 +66,6 @@ describe('GET /api/mail', function () {
       .set('Accept', 'application/json')
       .expect('Content-Type', /json/)
       .expect(function (res) {
-        // {
-        //   "status": 404,
-        //   "success": false,
-        //   "timestamp": "2021-06-12T09:55:07.000Z",
-        //   "value": {
-        //     "message": "... not found"
-        // }
-        // }
         assert(res.body.status === HttpStatus.statusNotFound);
         assert(res.body.success === false);
         assert(res.body.value.message.includes('not found'));
@@ -103,14 +86,6 @@ describe('POST /api/mail', function () {
       .set('Accept', 'application/json')
       .expect('Content-Type', /json/)
       .expect(function (res) {
-        // {
-        //   "status": 404,
-        //   "success": false,
-        //   "timestamp": "2021-06-12T09:55:07.000Z",
-        //   "value": {
-        //     "message": "... not found"
-        // }
-        // }
         assert(res.body.status === HttpStatus.statusServerError);
         assert(res.body.success === false);
         assert(res.body.value === HttpStatus.statusNameServerError);
@@ -119,43 +94,13 @@ describe('POST /api/mail', function () {
       .end(done);
   });
 
-  it('responds with success if message is defined', function (done) {
-    const message = {
-      from: 'team@damianos.io',
-      to: 'team@damianos.io',
-      subject: 'New user registration',
-      text: 'Hello world',
-    };
-
-    // (async () => {
-    //   try {
-    //     const res = await request.post('/api/mail').send({ message: message });
-    //     assert(res.body.status === HttpStatus.statusCreated);
-    //     assert(res.body.success === true);
-    //     assert(typeof res.body.value === 'object');
-    //     assert(res.body.value.accepted);
-    //     assert(res.body.value.accepted.length === 1);
-    //     assert(res.body.value.rejected);
-    //     assert(res.body.value.rejected.length === 0);
-    //     assert(res.body.value.response.includes('250 2.0.0 Ok: queued as'));
-    //     assert(res.body.value.envelope.from === message.from);
-    //     assert(res.body.value.envelope.to.length === 1);
-    //     assert(res.body.value.envelope.to[0] === message.to);
-    //     done();
-    //   } catch (err) {
-    //     done(err);
-    //   }
-    // })();
-
+  it('responds with success if message is defined - 1st way', function (done) {
     request
       .post('/api/mail')
       .send({ message: message })
       .set('Accept', 'application/json')
       .expect('Content-Type', /json/)
-      // .expect(HttpStatus.statusCreated)
       .then((res) => {
-        console.log('res.body.status: ', res.body.status);
-        console.log('res.body', res.body);
         assert(res.body.status === HttpStatus.statusCreated);
         assert(res.body.success === true);
         assert(typeof res.body.value === 'object');
@@ -170,5 +115,27 @@ describe('POST /api/mail', function () {
         done();
       })
       .catch((err) => done(err));
+  });
+
+  it('responds with success if message is defined - 2nd way', function (done) {
+    (async () => {
+      try {
+        const res = await request.post('/api/mail').send({ message: message });
+        assert(res.body.status === HttpStatus.statusCreated);
+        assert(res.body.success === true);
+        assert(typeof res.body.value === 'object');
+        assert(res.body.value.accepted);
+        assert(res.body.value.accepted.length === 1);
+        assert(res.body.value.rejected);
+        assert(res.body.value.rejected.length === 0);
+        assert(res.body.value.response.includes('250 2.0.0 Ok: queued as'));
+        assert(res.body.value.envelope.from === message.from);
+        assert(res.body.value.envelope.to.length === 1);
+        assert(res.body.value.envelope.to[0] === message.to);
+        done();
+      } catch (err) {
+        done(err);
+      }
+    })();
   });
 });
