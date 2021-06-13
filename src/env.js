c46657bb725502980c78ecd8949a3402fe441f12
1onlinesolution <team@damianos.io>
2021-06-13 11:07:22 +0100

    added @1onlinesolution/dws-utils

--- src/env.js
+++ src/env.js
@@ -1,15 +1,15 @@
-const { requireFromEnv } = require('./tools');
+const { getenv } = require('@1onlinesolution/dws-utils');
 
-const BODYPARSER_URLENCODED_LIMIT = requireFromEnv('BODYPARSER_URLENCODED_LIMIT', false) || '1mb';
-const BODYPARSER_JSON_LIMIT = requireFromEnv('BODYPARSER_JSON_LIMIT', false) || '1mb';
+const BODYPARSER_URLENCODED_LIMIT = getenv('BODYPARSER_URLENCODED_LIMIT', false) || '1mb';
+const BODYPARSER_JSON_LIMIT = getenv('BODYPARSER_JSON_LIMIT', false) || '1mb';
 
-const EMAIL_HOST = requireFromEnv('EMAIL_HOST');
-const EMAIL_USERNAME = requireFromEnv('EMAIL_USERNAME');
-const EMAIL_USERNAME2 = requireFromEnv('EMAIL_USERNAME2');
-const EMAIL_PASSWORD = requireFromEnv('EMAIL_PASSWORD');
+const EMAIL_HOST = getenv('EMAIL_HOST');
+const EMAIL_USERNAME = getenv('EMAIL_USERNAME');
+const EMAIL_USERNAME2 = getenv('EMAIL_USERNAME2');
+const EMAIL_PASSWORD = getenv('EMAIL_PASSWORD');
 
 
-const CORS_ORIGIN = requireFromEnv('CORS_ORIGIN', false);
+const CORS_ORIGIN = getenv('CORS_ORIGIN', false);
 const corsOptions = {};
 if (typeof CORS_ORIGIN !== 'undefined' && CORS_ORIGIN !== '') {
   corsOptions.origin = CORS_ORIGIN;
@@ -17,9 +17,9 @@ if (typeof CORS_ORIGIN !== 'undefined' && CORS_ORIGIN !== '') {
 
 
 module.exports = {
-  appName: requireFromEnv('APP_NAME', false) || 'Hermes Service',
-  env: requireFromEnv('NODE_ENV'),
-  port: parseInt(requireFromEnv('PORT', false) || 7001, 10),
+  appName: getenv('APP_NAME', false) || 'Hermes Service',
+  env: getenv('NODE_ENV'),
+  port: parseInt(getenv('PORT', false) || 7001, 10),
   corsOptions: corsOptions,
   bodyParser: {
     urlencodedLimit: BODYPARSER_URLENCODED_LIMIT,
